c52673e33197e8b42aca44f45eb90d58aa44190a
Nick Fausti <ncfausti@gmail.com>
2014-04-25 15:58:55 -0400

    added copy and transpose

--- python/copy_functions.py
+++ python/copy_functions.py
@@ -9,3 +9,11 @@ def setClipboard(data):
      p.stdin.write(data)
      p.stdin.close()
      retcode = p.wait()
+
+def transposeAndCopy(dataIds):
+    col = ''
+    ids = dataIds.split()
+    for i in range(len(ids)):
+        col+=ids[i]+'\n'
+    setClipboard(col)
+    return col
