ed43e9796f1bec0054e93c6c49871de31f6bc497
Nick Fausti <ncfausti@gmail.com>
2014-06-26 16:21:09 -0400

    added db update function example

--- python/database.py
+++ python/database.py
@@ -9,3 +9,35 @@ def getLocationsByPatientId():
             for(box, position, sampletype) in cursor:
                 f.write("{},{},{},{},{}\n".format(patIds[i], bmis[i],box, position, sampletype))
     cnx.close()
+
+
+
+
+### Update column on multiple rows in a database with some transformed value from another column
+
+#get list of values that you might want to use as a base to update
+
+cnx = mysql.connector.connect(user='root', database='cgi_ontology')
+cursor = cnx.cursor()
+q = "select DATE_OF_BIRTH, DATE_OF_BIRTH2 from `radr_baseline`"
+cursor.execute(q)
+
+#store values returned in a list
+oldDateList = []
+for (oldDate, somethingElse) in cursor:
+    oldDateList.append(oldDate)
+
+
+def updateRowsExample():
+    #create connection
+    cnx = mysql.connector.connect(user='root', database='cgi_ontology')
+    #set cursor
+    cursor = cnx.cursor()
+
+    # iterate through each item in local list and update each row in the database
+    for oldDate in oldDateList:
+        if not (oldDate == ""):
+            newYear = "19" + oldDate[-2:]
+            newDate = oldDate[:-2] + newYear
+            q = "UPDATE `radr_baseline` SET DATE_OF_BIRTH2=STR_TO_DATE('"+ newDate + "', '%d-%M-%Y') WHERE DATE_OF_BIRTH like '"+oldDate+"';"
+            cursor.execute(q)
