8b319810baa7a8b7609a310d0091f92fe786648d
Nick Fausti <ncfausti@gmail.com>
2014-08-20 16:18:10 -0400

    added string.format function

--- js/snippets.js
+++ js/snippets.js
@@ -1,1 +1,16 @@
 var printProgress = function(percentComplete) {var total = 100; for(i = 0; i < total;i++){ if(i <= percentComplete*total) {document.write("+");} else { document.write("="); } }}
+
+// String.format
+// usage 
+//'Your balance is {0} USD'.f(77.7) 
+// 'Added {0} by {1} to your collection'.f(title, artist)
+//
+String.prototype.format = String.prototype.f = function() {
+    var s = this,
+        i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
