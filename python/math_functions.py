1143f5bf055530d586e436965e9a8c5aa120c62b
Nick Fausti <ncfausti@gmail.com>
2015-01-08 00:43:24 -0500

    added int to binary conversion algorithm

--- python/math_functions.py
+++ python/math_functions.py
@@ -4,3 +4,22 @@ def gcd(p, q):
 		return p
 	else:
 		return gcd(q, p % q)
+
+def convert_int_to_binary(n):
+	stack = []  				# Use a list data structure for our stack
+	binary_value = ""
+
+	while n > 0:
+		stack.append(n % 2)  	# append == push
+		n = n / 2
+
+	stack.reverse()				# Use as LIFO instead of FIFO
+	
+	for number in stack:
+		binary_value += str(number)
+
+
+	print(binary_value)
+
+#print gcd(35, 21)
+#convert_int_to_binary(50)
