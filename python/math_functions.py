22e8470f2d475ed4529327323264d1abf491267e
Nick Fausti <ncfausti@gmail.com>
2022-08-27 12:53:23 -0400

    Add increasing decimal precision function.

--- python/math_functions.py
+++ python/math_functions.py
@@ -1,25 +1,26 @@
 # Greatest common devisor
 def gcd(p, q):
-	if q == 0:
-		return p
-	else:
-		return gcd(q, p % q)
+    if q == 0:
+        return p
+    else:
+        return gcd(q, p % q)
+
 
 def convert_int_to_binary(n):
-	stack = []  				# Use a list data structure for our stack
-	binary_value = ""
+    stack = []  				# Use a list data structure for our stack
+    binary_value = ""
 
-	while n > 0:
-		stack.append(n % 2)  	# append == push
-		n = n / 2
+    while n > 0:
+        stack.append(n % 2)  	# append == push
+        n = n / 2
 
-	stack.reverse()				# Use as LIFO instead of FIFO
-	
-	for number in stack:
-		binary_value += str(number)
+    stack.reverse()				# Use as LIFO instead of FIFO
+
+    for number in stack:
+        binary_value += str(number)
+
+    print(binary_value)
 
 
-	print(binary_value)
-
-#print gcd(35, 21)
-#convert_int_to_binary(50)
+def nines(x, precision):
+    return x + sum([10**(i*-1) for i in range(1, precision+1)])
